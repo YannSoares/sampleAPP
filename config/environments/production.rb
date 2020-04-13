7288e04f65adc3ca6381f0c53a5827f701d2d4ea
Yann <yann.rocha932@gmail.com>
2020-04-13 17:07:41 -0300

    add send username e password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,8 +69,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['app165367068@heroku.com'],
+    :password => ENV['cczvacod7522'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
