6790918276e5fa16f45a4b60b7bd62a684181607
Yann <yann.rocha932@gmail.com>
2020-04-14 11:05:52 -0300

    arrumando erro envio email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "sampleAPP_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   host = 'sample-app0ruby001.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
@@ -69,8 +69,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['app165367068@heroku.com'],
-    :password => ENV['cczvacod7522'],
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
